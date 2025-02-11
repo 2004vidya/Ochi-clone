f095a8fce552bda76becb4412487dbc30c620dab
2004vidya <pandey.vidya013@gmail.com>
2025-02-11 21:36:47 +0530

    Marquee and navbar

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { motion } from "motion/react";
+
+const Marquee = () => {
+  return (
+    <div data-scroll data-scroll-speed =".1" data-scroll-section className="w-full text-white  py-20 mt-10 rounded-tl-3xl rounded-tr-3xl  bg-[#004d43]">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap  overflow-hidden">
+        <motion.h1
+          initial={{ x: "0" }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", duration: 10, repeat: Infinity }}
+          style={{ fontFamily: "f1" }}
+          className="text-[24vw] leading-none font-semibold uppercase -mb-[6vw] pt-10 pr-20 "
+        >
+          We are ochi{" "}
+        </motion.h1>
+        <motion.h1
+          initial={{ x: "0" }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", duration: 10, repeat: Infinity }}
+          style={{ fontFamily: "f1" }}
+          className="text-[24vw] leading-none font-semibold uppercase -mb-[6vw] pt-10 pr-20 "
+        >
+          We are ochi{" "}
+        </motion.h1>
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
